655e24642478da423d4535ed77e32143e89f404d
DarkSide <gl1207r@gmail.com>
2025-07-06 20:31:56 -0300

    Arquivos

---
+++ fxmanifest.lua
@@ -0,0 +1,47 @@
+                    ---------------------------------------------
+                    --      LOADSCREEN DESENVOLVIDA POR        --
+                    --          NUBLADO & DARKSIDE             --
+                    --            VENDA PROIBIDA               --
+                    --  CASO USAR A LOADSCREEN EM OPEN SOURCE  --
+                    --            ATRIBUIR CRÉDITOS            --
+                    --           EM ALGUM README.md            --
+                    --                                         --
+                    --        © DARKSIDE & NUBLADO 2025        --
+                    ---------------------------------------------
+
+
+fx_version "bodacious"        --VERSÃO
+game "gta5"       --GAME QUE VAI USAR
+
+-------------------------ARQUIVOS PARA SEREM CARREGADOS--------------------------
+
+files {
+    --ARQUIVOS
+    'darkside.html',
+    'lightsaber.js',
+    'theforce.css',
+    --SONS
+	'songs/duas_da_manha.mp3',
+    'songs/INNERBLOOM.mp3',
+    'songs/pirocada.mp3',
+    'songs/sc.mp3',
+    'songs/tipo_nino.mp3',
+    'songs/viver_o_momento.mp3',
+    --IMAGENS
+	'images/logo.png',
+    'images/background.jpg',
+    'images/duas_da_manha.jpg',
+    'images/innerbloom.jpg',
+    'images/pause.png',
+    'images/pirocada_quente.jpg',
+    'images/play.png',
+    'images/proximo.png',
+    'images/sc.jpg',
+    'images/tipo_nino.jpg',
+    'images/viver_o_momento.jpg',
+    'images/voltar.png'
+}
+
+---------------------O ARQUIVO DA LOADSCREEN-------------------------
+
+loadscreen 'darkside.html'
